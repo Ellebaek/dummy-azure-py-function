5de76fe905da180cea7d19f6eb959990823f8c62
Thomas Ellebæk <thel@ferring.com>
2022-01-12 22:28:27 +0000

    updated timer function

--- dummy_function04/__init__.py
+++ dummy_function04/__init__.py
@@ -3,11 +3,11 @@ import logging
 import azure.functions as func
 
 
-def main(timer_var: func.TimerRequest) -> None:
+def main(tvar: func.TimerRequest) -> None:
     # utc_timestamp = datetime.datetime.utcnow().replace(
     #    tzinfo=datetime.timezone.utc).isoformat()
 
     #if timer_var.past_due:
     #    logging.info('dummy function 04 timer is past due!')
 
-    logging.info('dummy function 04 ran (timer trigger) at {0}', datetime.datetime.now())
+    logging.info('dummy function 04 ran (timer trigger) at {0}'.format(datetime.datetime.now()))
